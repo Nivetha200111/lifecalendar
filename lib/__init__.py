16a47234576852e9d4a5e3ac7482861202196a8b
Nivetha200111 <nnivetha373@gmail.com>
2026-01-19 19:23:57 +0530

    Fix: Remove unused imports from lib

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,7 +1,5 @@
-"""Vaporwave wallpaper generation library."""
+"""Minimalist wallpaper generation library."""
 
 from .wallpaper import generate_wallpaper, generate
-from .elements import COLORS
-from .effects import apply_all_effects
 
-__all__ = ['generate_wallpaper', 'generate', 'COLORS', 'apply_all_effects']
+__all__ = ['generate_wallpaper', 'generate']
